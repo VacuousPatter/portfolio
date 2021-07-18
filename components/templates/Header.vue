07d8edf09950c61533d3b2db11bb8d38be254451
Maycon Pinto <mayconjesus07@gmail.com>
2021-07-18 00:52:50 -0300

    Componente header adicionado

---
+++ components/templates/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <vs-navbar v-model="active" fixed color="primary" text-white>
+      <template #left>
+        Maycon Pinto
+        <!-- <img src="/logo2.png" alt=""> -->
+      </template>
+      <vs-navbar-item to="/" :active="$route.path=='/'">
+        Início
+      </vs-navbar-item>
+      <vs-navbar-item to="/sobre" :active="$route.path=='/sobre'">
+        Sobre
+      </vs-navbar-item>
+    </vs-navbar>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+header{
+height: 44px;
+}
+</style>
