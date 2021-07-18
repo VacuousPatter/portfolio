65519282d9a5bd3fb2233e7c715db0e4969f2c9f
Maycon Pinto <mayconjesus07@gmail.com>
2021-07-18 16:19:50 -0300

    Componente header

--- components/templates/Header.vue
+++ components/templates/Header.vue
@@ -1,28 +1,163 @@
 <template>
   <header>
-    <vs-navbar v-model="active" fixed color="primary" text-white>
-      <template #left>
-        Maycon Pinto
-        <!-- <img src="/logo2.png" alt=""> -->
-      </template>
-      <vs-navbar-item to="/" :active="$route.path=='/'">
-        Início
-      </vs-navbar-item>
-      <vs-navbar-item to="/sobre" :active="$route.path=='/sobre'">
-        Sobre
-      </vs-navbar-item>
-    </vs-navbar>
+    <div class="header-wrapper">
+      <div class="name">
+        <nuxt-link class="link-name" to="/">
+          <span>
+            <strong>Maycon</strong> Pinto
+          </span>
+        </nuxt-link>
+      </div>
+      <button class="menu" @click="navOpen=!navOpen">
+        <Icon icon="mdi:menu" />
+      </button>
+      <nav ref="nav">
+        <div class="nav-wrapper">
+          <TemplatesNavItem to="/sobre" @closeNav="closeNav">
+            Sobre
+          </TemplatesNavItem>
+          <TemplatesNavItem to="/servicos" @closeNav="closeNav">
+            Serviços
+          </TemplatesNavItem>
+          <TemplatesNavItem to="/conhecimentos" @closeNav="closeNav">
+            Conhecimentos
+          </TemplatesNavItem>
+          <TemplatesNavItem to="/projetos" @closeNav="closeNav">
+            Projetos
+          </TemplatesNavItem>
+          <TemplatesNavItem to="/contato" @closeNav="closeNav">
+            Contato
+          </TemplatesNavItem>
+        </div>
+      </nav>
+    </div>
   </header>
 </template>
 
 <script>
-export default {
+import { Icon } from '@iconify/vue2'
 
+export default {
+  components: { Icon },
+
+  data () {
+    return {
+      navOpen: false
+    }
+  },
+
+  watch: {
+    navOpen (val) {
+      const element = this.$refs.nav
+      if (val) {
+        const sectionHeight = element.scrollHeight
+        element.style.height = `${sectionHeight}px`
+
+        function onTransitionEnd (e) {
+          element.removeEventListener('transitionend', onTransitionEnd)
+
+          element.style.height = 'auto'
+        }
+
+        element.addEventListener('transitionend', onTransitionEnd)
+
+        element.setAttribute('data-collapsed', 'false')
+      } else {
+        const sectionHeight = element.scrollHeight
+        requestAnimationFrame(function () {
+          element.style.height = sectionHeight + 'px'
+
+          requestAnimationFrame(function () {
+            element.style.height = null
+          })
+        })
+
+        element.setAttribute('data-collapsed', 'true')
+      }
+    }
+  },
+
+  methods: {
+    closeNav () {
+      this.navOpen = false
+    }
+  }
 }
 </script>
 
 <style scoped>
 header{
-height: 44px;
+  background-color: #000814;
+  color: #f0efeb;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+}
+
+.header-wrapper{
+  padding: 15px 15px;
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  width: 75vw;
+}
+
+header .name{
+  font-size: 1.2rem;
+}
+
+header .name .link-name{
+  text-decoration: none;
+  color: inherit;
+}
+
+nav{
+  margin-left: auto;
+}
+
+.nav-wrapper{
+  display: flex;
+  flex-flow: row nowrap;
+}
+
+button.menu{
+  display: none;
+  background: none;
+  border: none;
+  color: #FFC300;
+  background: transparent;
+
+  padding: 0px;
+  font-size: 26px;
+  box-sizing: border-box;
+  border-radius: 999rem;
+  padding: 5px;
+  height: 36px;
+  width: 36px;
+  margin-left: auto;
+}
+
+@media (max-width:950px) {
+  .header-wrapper{
+    padding: 12px 15px;
+    flex-flow: row wrap;
+  }
+
+  button.menu{
+    display: block;
+  }
+
+  nav{
+    height: 0px;
+    transition: all 0.3s 0s;
+    overflow: hidden;
+  }
+
+  .nav-wrapper{
+    width: 75vw;
+    flex-flow: column nowrap;
+    padding: 10px 0px;
+    padding-top: 10px;
+  }
 }
 </style>
---
+++ components/templates/NavItem.vue
@@ -0,0 +1,70 @@
+<template>
+  <nuxt-link class="link" :to="to" :active="$route.path === to" @click.native="$emit('closeNav')">
+    <slot name="default" />
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  props: {
+    to: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.link{
+  text-decoration: none;
+  color: #DCDAD0;
+  font-weight: 500;
+  margin: 0px 4px;
+  padding: 0px 6px;
+  position: relative;
+  transition: transform 0.2s;
+  position: relative;
+}
+
+.link:hover,
+.link[active]{
+transform: translateY(-2px);
+}
+
+.link[active]::after{
+  content: "";
+  display: block;
+  height: 5px;
+  width: 5px;
+  background: #FFD60A;
+  border-radius: 999rem;
+  left: 50%;
+  bottom: -5px;
+  position: absolute;
+  transform: translate(0px,-50%);
+}
+
+.link[active]{
+  color: #FFD60A;
+}
+
+@media (max-width:950px) {
+  .link{
+    margin-left: 10px !important;
+  }
+  .link:first-child{
+    margin-top: 0px;
+  }
+  .link:last-child{
+    margin-bottom: 0px;
+  }
+  .link{
+    margin: 5px 0px;
+  }
+  .link[active]::after{
+    left: -5px;
+    top: 50%;
+  }
+}
+</style>
