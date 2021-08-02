fd3ce46fc1374739b01bba5dc5a3c3440b2c36d0
Maycon Pinto <mayconjesus07@gmail.com>
2021-08-01 22:29:29 -0300

    Paginas em construção

---
+++ components/templates/PaginaEmConstrucao.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="wrapper">
+    <img class="astronauta-trabalhando" src="~/assets/images/astronautas/astronauta-trabalhando.png" alt="Astronauta trabalhando">
+    <div class="content">
+      <h1>Página em construção!</h1>
+      <p>Lamentamos informar mas essa página está em construção! Volte aqui mais tarde para ver ela pronta!</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.wrapper{
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh - 65px);
+}
+
+.astronauta-trabalhando{
+  height: 60vh;
+}
+
+.content{
+  height: 60vh;
+  max-width: 500px;
+}
+</style>
---
+++ pages/contato.vue
@@ -0,0 +1,4 @@
+<script>
+import PaginaEmConstrucao from '../components/templates/PaginaEmConstrucao.vue'
+export default PaginaEmConstrucao
+</script>
