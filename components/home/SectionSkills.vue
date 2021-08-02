4ab0678f521fb361622fe78feec98afc209d15a2
Maycon Pinto <mayconjesus07@gmail.com>
2021-08-01 22:03:33 -0300

    Skills

--- components/home/SectionSkills.vue
+++ components/home/SectionSkills.vue
@@ -2,20 +2,112 @@
   <section>
     <Title title="Skills" />
 
-    <p>Em desenvolvimento!</p>
+    <div class="container">
+      <div class="card container-formacao">
+        <img class="astronauta-lendo" height="100" src="~/assets/images/astronautas/astronauta-lendo-2.png" alt="Astronauta lendo">
+        <div class="card-title">
+          <h3>Formação</h3>
+        </div>
+        <div class="card-content">
+          <FormacaoItem v-for="(course,i) in courses" :key="i" :course="course" />
+        </div>
+      </div>
+      <div class="card container-skills">
+        <img class="astronauta-segurando-estrela" height="150" src="~/assets/images/astronautas/astronauta-segurando-estrela.png" alt="Astronauta segurando estrela">
+
+        <div class="card-title">
+          <h3>Skills</h3>
+        </div>
+        <div class="card-content">
+          <p>Em desenvolvimento...</p>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
 import Title from '../../components/templates/Title.vue'
+import FormacaoItem from './skills/FormacaoItem.vue'
+import courses from '~/assets/data/courses.json'
 
 export default {
-  components: { Title }
+  components: { Title, FormacaoItem },
+
+  data () {
+    return {
+      courses,
+
+      showAnimation: {
+        formacao: false,
+        skills: false
+      }
+    }
+  }
 }
 </script>
 
 <style scoped>
 section{
   padding: 50px 25px;
+}
+
+.container{
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: center;
+  margin-top: 37.5px;
+}
+
+.container-formacao,
+.container-skills{
+  position: relative;
+}
+
+.container-formacao .astronauta-lendo{
+  position: absolute;
+  top: -100px;
+  left: -10px;
+}
+
+.container-skills .astronauta-segurando-estrela{
+  position: absolute;
+  right: -75px;
+  bottom: -50px;
+}
+
+@media (max-width:700px) {
+  .container-skills .astronauta-segurando-estrela{
+    height: 100px;
+    right: -17px;
+    bottom: 0px;
+  }
+}
+
+.card{
+  padding: 25px;
+  margin: 12.5px;
+  box-sizing: border-box;
+  border: 2px solid var(--secondary);
+  border-radius: 5px;
+  background: rgba(0,0,0,.15);
+  width: 515px;
+  align-self: stretch;
+  transition: transform 0.3s, border-color 0.3s;
+}
+
+.card:hover{
+  transform: translateY(-5px);
+  border-color: var(--primary);
+}
+
+.card .card-title{
+  font-size: 24px;
+  font-weight: bold;
+}
+
+.card .card-title h3{
+  margin: 0px;
 }
 </style>
---
+++ components/home/skills/FormacaoItem.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="list-item">
+    <a v-if="course.url" :href="course.url" target="_blank" class="list-item-title">
+      {{ course.name }}
+    </a>
+    <p v-else class="list-item-title">
+      {{ course.name }}
+    </p>
+
+    <div class="list-item-content">
+      <a class="school" :href="course.school.url" target="_blank">
+        <p>
+          {{ course.school.name }}
+        </p>
+      </a>
+
+      <a v-if="course.professor" class="professor before-point" :href="course.professor.url" target="_blank">{{ course.professor.name }}</a>
+
+      <a v-if="course.certificate" class="link-certificado before-point" :href="course.certificate.url" target="_blank">Ver Certificado</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    course: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.list-item{
+  padding: 15px 30px;
+  margin-top: 15px;
+  display: flex;
+  flex-flow: column nowrap;
+}
+
+.list-item-content{
+  margin-top: 5px;
+}
+
+p{
+  margin: 0;
+  display: inline;
+}
+
+/* REMOVER ESTILOS DE LINKS */
+.professor,
+.link-certificado,
+.school,
+.list-item-title{
+  text-decoration: none;
+  color: inherit;
+}
+
+.list-item-title{
+  font-weight: 500;
+  font-size: 18px;
+}
+
+.list-item-content{
+  font-size: 14px;
+}
+
+.school{
+  color: var(--primary);
+}
+
+.link-certificado{
+  font-weight: 500;
+}
+
+.before-point::before{
+  content: " • ";
+}
+</style>
